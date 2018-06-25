31d92065c20ca117e66c67a4ff84b3efe3bda53e
neiromendez <alne1990@hotmail.com>
2018-06-25 18:00:41 -0400

    imprimiendo en pantalla y acomodando el endpoint por el pedido en la prueba /profile/facebook/{id}

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,10 @@
 <?php
 
-$app->get('/id/{id}', function ($request, $response, $args) use ($app) {
-
+$app->get('/profile/facebook/{id}', function ($request, $response, $args) use ($app) {
 
     $facebook_profile_id = $args['id'];
 
-    $fb = new \Facebook\Facebook([
+    $fb = new Facebook\Facebook([
         'app_id' => '246293189462608',
 
         'app_secret' => 'fd53f34bb0ef872c26a02a2a431cd5a8',
@@ -15,19 +14,22 @@ $app->get('/id/{id}', function ($request, $response, $args) use ($app) {
 
     try {
         $response = $fb->get('/' . $facebook_profile_id . '?fields=id,name,first_name,last_name', 'EAADgAJoRilABAOI4K9ymPZBhZCat4guNAxf0VLbr3JtrZCcXowahXGXHZADAUZAd8eIZCij3piu7cSGtL4gjQlSWeZAHZAI2X1FnRjP3OKbrK4azfJ7DTP2RH6nLY2YPXrsvXqnvTZBDD4sXgEukvj5vuloDHkkDQFZCAZD');
-    } catch(\Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
         echo 'Graph returned an error: ' . $e->getMessage();
+
         exit;
-    } catch(\Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
         echo 'Facebook SDK returned an error: ' . $e->getMessage();
+
         exit;
     }
 
-    $me = $response->getGraphUser();
-    echo 'Logged in as ' . $me->getName();
+    $user = $response->getGraphUser();
 
 
+    echo 'Name: ' . $user['name'];
 
+    echo '<br>';
+
+    echo $user;
 });
